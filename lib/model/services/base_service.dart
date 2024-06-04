08f8de4599fd4b1aa3f2b9d4b60e064269091e1b
rezaagung1101 <rezaagung3910@gmail.com>
2024-06-04 20:01:23 +0700

    add: api services

--- lib/model/services/base_service.dart
+++ lib/model/services/base_service.dart
@@ -1,5 +1,7 @@
 abstract class BaseService{
-  final String baseURL = "";
+  final String baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+  // final String appId = "7094d3991048d0bfc1407d35dee9a0a3";
+  final String appIdPath = "&appid=7094d3991048d0bfc1407d35dee9a0a3";
 
   Future<dynamic> getResponse(String url);
 }
--- lib/model/services/media_service.dart
+++ lib/model/services/media_service.dart
@@ -1,10 +1,45 @@
-import 'package:openweather_mvvm/model/services/base_service.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class MediaService extends BaseService{
+import 'package:flutter/cupertino.dart';
+import 'package:openweather_mvvm/model/api/app_exception.dart';
+import 'package:openweather_mvvm/model/services/base_service.dart';
+import 'package:openweather_mvvm/utils/constants.dart';
+import 'package:http/http.dart' as http;
+
+
+class MediaService extends BaseService {
+
   @override
-  Future getResponse(String url) {
-    // TODO: implement getResponse
-    throw UnimplementedError();
+  Future getResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response = await http.get(
+          Uri.parse(baseURL + Constants.city + appIdPath));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+  @visibleForTesting
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while communication with server' +
+                ' with status code : ${response.statusCode}');
+    }
   }
 
 }
