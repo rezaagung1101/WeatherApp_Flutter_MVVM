a032ffe50d5ed8c9c114375f4e9dd6fdcadd2f86
rezaagung1101 <rezaagung3910@gmail.com>
2024-06-06 15:06:39 +0700

    add: main section

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:openweather_mvvm/utils/constants.dart';
 import 'package:openweather_mvvm/view/widgets/information_card_section.dart';
+import 'package:openweather_mvvm/view/widgets/main_section.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,20 +18,31 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Home Page"),
       ),
       body: Container(
-          color: Constants.skyBlue,
-          child: const SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                InformationCardSection(
-                    sunrise: "06.30",
-                    sunset: "17.39",
-                    wind: "70kmh",
-                    pressure: "0.5",
-                    humidity: "0.0",
-                    info: "Data")
-              ],
-            ),
-          )),
+        color: Constants.skyBlue,
+        // child: const SingleChildScrollView(
+        child: const Center(
+          child: Column(
+            children: <Widget>[
+              MainSection(
+                  mainTemp: "0.0",
+                  minTemp: "0.0",
+                  maxTemp: "0.0",
+                  status: "Cloudy af"),
+              SizedBox(
+                height: 32,
+              ),
+              InformationCardSection(
+                  sunrise: "06.30",
+                  sunset: "17.39",
+                  wind: "70kmh",
+                  pressure: "0.5",
+                  humidity: "0.0",
+                  info: "Data")
+            ],
+          ),
+        ),
+        // )
+      ),
     );
   }
 }
---
+++ lib/view/widgets/main_section.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+// class MainSection extends StatefulWidget {
+//   const MainSection(
+//       {super.key, required this.mainTemp, required this.minTemp, required this.maxTemp, required this.desc});
+//
+//   final String mainTemp;
+//   final String minTemp;
+//   final String maxTemp;
+//   final String desc;
+//
+//   @override
+//   State<MainSection> createState() => _MainSectionState();
+// }
+//
+// class _MainSectionState extends State<MainSection> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: <Widget>[
+//           Row(
+//             Text(widget.)
+//           )
+//         ],
+//       ),
+//     )
+//   }
+// }
+
+class MainSection extends StatelessWidget {
+  const MainSection(
+      {super.key,
+      required this.mainTemp,
+      required this.minTemp,
+      required this.maxTemp,
+      required this.status});
+
+  final String mainTemp;
+  final String minTemp;
+  final String maxTemp;
+  final String status;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(width: 48,),
+              TextSection(
+                text: mainTemp,
+                size: 90,
+              ),
+              const Align(
+                  alignment: Alignment.topLeft,
+                  child: TextSection(text: "°C", size: 40)),
+            ],
+          ),
+          TextSection(text: status, size: 20),
+          const SizedBox(height: 4,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextSection(text: "Min Temp:$minTemp°C", size: 16),
+              const SizedBox(width: 16,),
+              TextSection(text: "Max Temp:$maxTemp°C", size: 16),
+            ],
+          )
+        ],
+      );
+  }
+}
+
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.text, required this.size});
+
+  final String text;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'poppins',
+        color: Colors.white,
+        fontSize: size,
+      ),
+    );
+  }
+}
